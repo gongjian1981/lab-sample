33f7c9b9643e7b2484ee16cfb6328848b2785b3c
Jian Gong <gongjian1981@gmail.com>
2025-06-15 02:54:47 -0400

    first merge

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,13 +1,19 @@
-import Homepage from '../HomePage'; 
+import Homepage from '../HomePage'; // 👈 import the new homepage component
+import heatmap from './heatmap';
+import daily_view from './daily_view';
+import upload_evaluation from './upload_evaluation';
 import Layout from '../Enrollment/Layout/Layout';
 import studentRoutes from './student';
 import upload_student from './upload_student';
 
 export const appRoutes = [
- {
+  {
     path: '/',
-    element: <Homepage />,
+    element: <Homepage />, // 👈 use the new component here
   },
+  heatmap,
+  daily_view,
+  upload_evaluation,,
   {
     path: '/',
     element: <Layout />,
